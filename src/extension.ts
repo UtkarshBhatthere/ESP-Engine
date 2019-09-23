d9bee2ca43538416a4e4c56179340a5b0aa58cc7
binggee <binggee@foxmail.com>
2019-09-23 18:15:35 +0800

    initiate_command download zip and unzip;

--- src/extension.ts
+++ src/extension.ts
@@ -1,75 +1,166 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as compressing from 'compressing';
+import * as https from 'https';
+import * as path from 'path';
+import * as url from 'url';
 import * as vscode from 'vscode';
+import * as fs from "fs-extra";
+function download(urlstring: string, stream: fs.WriteStream) {
+	return new Promise((resolve, rejects) => {
+		let urlload = url.parse(urlstring);
+		stream.on('close', () => {
+			resolve();
+		});
+		https.get(urlload, (res) => {
+			console.log('statusCode :' + res.statusCode);
+			if (res.statusCode === 302) {
+				if (res.headers['location']) {
+					download(res.headers['location'], stream);
+				}
+			} else if (res.statusCode === 200) {
+				res.pipe(stream);
+			} else {
+				rejects(res.statusCode);
+			}
+		});
+	});
+}
 
+function ungzip(src: string, dir: string) {
+	return new Promise((resolve, rejects) => {
+		fs.stat(src, (err, stats) => {
+			if (err) {
+				rejects(err);
+			}
+			if (stats.isFile()) {
+				compressing.zip.uncompress(src, dir).finally(() => {
+					resolve();
+				});
+			} else {
+				rejects(stats);
+			}
+		});
+	});
+}
+function getallfilepath(rootpath: string) {
+	fs.readdir(rootpath, (err, files) => {
+		if (err) {
+			console.log(err);
+		}
+		files.forEach((val, key) => {
+			if (fs.statSync(val).isFile()) {
+				fs.move(path.resolve(rootpath, "esp-idf-template-master", val), path.resolve(rootpath, val)).finally(() => {
+				});
+			}
+			else {
+			}
+		});
+	});
+}
+function movefiles(rootpath: string) {
+	fs.readdir(rootpath, (err, files) => {
+		if (err) {
+			console.log(err);
+		}
+		let index = files.indexOf("esp-idf-template-master");
+		if (index !== undefined) {
+			console.log(files[index]);
+			let allfilepath = path.resolve(rootpath, "esp-idf-template-master");
+			getallfilepath(allfilepath);
+
+
+			// items.forEach(element => {
+			// 	console.log(path.resolve(rootpath, "esp-idf-template-master", element));
+			// 	fs.moveSync(path.resolve(rootpath, "esp-idf-template-master", element),
+			// 		path.resolve(rootpath, element));
+			// });
+
+		}
+		else {
+			console.log("can't find floader");
+		}
+	});
+}
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('ESP-IDF engine is now active!');
 	let activity_status = true;
 	let engine_terminal = vscode.window.createTerminal("esp-engine");
-	let template_uri = "'https://github.com/espressif/esp-idf-template/archive/master.zip'";
+	let zipfileurl = 'https://github.com/espressif/esp-idf-template/archive/master.zip';
+	let zipfilename = 'master.zip';
 
+
+	let IDF_PATH: string;
+	if (process.platform === ("win32")) {
+		IDF_PATH = "$env:IDF_PATH/tools/idf.py";
+	} else {
+		IDF_PATH = "$IDF_PATH/tools/idf.py";
+	}
 	// Initiate Command
 	// Not completely implemented
-	let initiate_command = vscode.commands.registerCommand('esp-engine.initiate', async function (){
+	let initiate_command = vscode.commands.registerCommand('esp-engine.initiate', async function () {
 		// Init Message
-		vscode.window.showInformationMessage('Initiating Directory as ESP-IDF project ... It will take a few seconds.');
-		// Setting up CWD as a ESP-IDF template.
-		// engine_terminal = vscode.window.createTerminal('esp-Init');
-		await engine_terminal.sendText("$loc = Get-Location", true);
-		await engine_terminal.sendText("mkdir $loc\\.esp-engine", true);
-		await engine_terminal.sendText("Invoke-WebRequest -Uri " + template_uri + " -OutFile $loc\\.esp-engine\\template.zip", true);
-		await engine_terminal.sendText("Expand-Archive $loc\\.esp-engine\\template.zip $loc\\.esp-engine\\", true);
-		await engine_terminal.sendText("mv $loc\\.esp-engine\\esp-idf-template-master\\* $loc\\", true);
-		await engine_terminal.sendText("Remove-Item $loc\\.esp-engine\\esp-idf-template-master -Force -Recurse");
+		// vscode.window.showInformationMessage('Initiating Directory as ESP-IDF project ... It will take a few seconds.');
+		console.log("running!");
+		if (vscode.workspace.workspaceFolders) {
+			let rootpath = path.normalize(vscode.workspace.workspaceFolders[0].uri.fsPath);
+			let zipfliepath = path.resolve(rootpath, zipfilename);
+			let writer = fs.createWriteStream(zipfliepath);
+			await download(zipfileurl, writer);
+			await ungzip(zipfliepath, rootpath);
+			movefiles(rootpath);
+		} else {
+			vscode.window.showInformationMessage('Please open a floader');
+		}
+		console.log("done!");
 	});
 	context.subscriptions.push(initiate_command);
 
 	// Build Command
-	let build_command = vscode.commands.registerCommand('esp-engine.build', function(){
-		if(activity_status){
+	let build_command = vscode.commands.registerCommand('esp-engine.build', function () {
+		if (activity_status) {
 			console.log("'CMakeLists.txt' file is present, initiating build.");
 			// engine_terminal = vscode.window.createTerminal("esp-build");
 			vscode.workspace.saveAll();
 			engine_terminal.show();
 			vscode.window.showInformationMessage("Project Compilation Initiated.");
-			engine_terminal.sendText("python $env:IDF_PATH/tools/idf.py build", true);
+			engine_terminal.sendText("python " + IDF_PATH + " build", true);
 		}
 	});
 	context.subscriptions.push(build_command);
 
 	// Menuconfig Command
-	let menuconfig_command = vscode.commands.registerCommand('esp-engine.config', function(){
-		if(activity_status){
+	let menuconfig_command = vscode.commands.registerCommand('esp-engine.config', function () {
+		if (activity_status) {
 			// engine_terminal = vscode.window.createTerminal("esp-config");
 			engine_terminal.show();
 			vscode.window.showInformationMessage("Terminal size should be atleast 19 Lines by 80 Columns");
-			engine_terminal.sendText("python $env:IDF_PATH/tools/idf.py menuconfig", true);
-			
+			engine_terminal.sendText("python " + IDF_PATH + " menuconfig", true);
+
 		}
 	});
 	context.subscriptions.push(menuconfig_command);
 
 	// Clean Command
-	let clean_command = vscode.commands.registerCommand('esp-engine.clean', async function(){
-		if(activity_status){
+	let clean_command = vscode.commands.registerCommand('esp-engine.clean', async function () {
+		if (activity_status) {
 			// engine_terminal = vscode.window.createTerminal("esp-clean");
 			vscode.window.showInformationMessage("Cleaning built files.");
-			await engine_terminal.sendText("python $env:IDF_PATH/tools/idf.py clean", true);
+			await engine_terminal.sendText("python " + IDF_PATH + " clean", true);
 			vscode.window.showInformationMessage("We've cleaned the built files.");
 		}
 	});
 	context.subscriptions.push(clean_command);
 
 	// Flash command
-	let flash_command = vscode.commands.registerCommand('esp-engine.flash', async function(){
-		if(activity_status){
+	let flash_command = vscode.commands.registerCommand('esp-engine.flash', async function () {
+		if (activity_status) {
 			// engine_terminal = vscode.window.createTerminal("esp-flash");
-			vscode.window.showInformationMessage("Flashing Target", );
+			vscode.window.showInformationMessage("Flashing Target");
 			let baudrate = await vscode.window.showInputBox({
 				prompt: "Specify Baudrate.",
 				value: "115200"
@@ -79,37 +170,27 @@ export function activate(context: vscode.ExtensionContext) {
 				value: "COM10"
 			});
 			await engine_terminal.show();
-			await engine_terminal.sendText("python $env:IDF_PATH/tools/idf.py -p " + port + " -b " + baudrate + " flash", true);
+			await engine_terminal.sendText("python " + IDF_PATH + " -p " + port + " -b " + baudrate + " flash", true);
 		}
 	});
 	context.subscriptions.push(flash_command);
 
 	// Monitor command
-	let monitor_command = vscode.commands.registerCommand('esp-engine.monitor', function(){
-		if(activity_status){
+	let monitor_command = vscode.commands.registerCommand('esp-engine.monitor', function () {
+		if (activity_status) {
 			// engine_terminal = vscode.window.createTerminal("esp-monitor");
 			vscode.window.showInformationMessage("Opening Serial Monitor");
 			engine_terminal.show();
-			engine_terminal.sendText("python $env:IDF_PATH/tools/idf.py monitor", true);
+			engine_terminal.sendText("python " + IDF_PATH + " monitor", true);
 		}
 	});
 	context.subscriptions.push(monitor_command);
-
-	// Setup Command
-	// let setup_command = vscode.commands.registerCommand('esp-engine.monitor', function(){
-	// 	if(activity_status){
-	// 		engine_terminal = vscode.window.createTerminal("esp-monitor");
-	// 		vscode.window.showInformationMessage("Opening Serial Monitor");
-	// 		engine_terminal.show();
-	// 		engine_terminal.sendText("python $env:IDF_PATH/tools/idf.py monitor", true);
-	// 	}
-	// });
-	// context.subscriptions.push(setup_command);
 }
 exports.activate = activate;
 
+
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
